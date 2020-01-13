374539b71853f8e1221e032eceac832f0838be2a
satishppawar <pawar.satish2345@gmail.com>
2020-01-13 22:59:41 +0530

    first Java project

---
+++ 01-CollectionDemo/src/demo/basicJava/collection/ArrayListDemo.java
@@ -0,0 +1,12 @@
+package demo.basicJava.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<String> arrayList=new ArrayList<>();
+		arrayList.add(new String());
+		arrayList.add(new String());
+		System.out.println(arrayList.size());
+	}
+}

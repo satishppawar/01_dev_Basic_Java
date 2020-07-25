22d7747017e416637877fea742f868f0b0bbcff7
satishppawar <pawar.satish2345@gmail.com>
2020-07-25 09:41:39 +0530

    Java Opp

---
+++ Java8/src/com/java8/lambda1/intro/Greeter.java
@@ -0,0 +1,23 @@
+
+package com.java8.lambda1.intro;
+
+/**
+ * @author Satish Pawar
+ *
+ */
+public class Greeter {
+
+	public void greet() {
+		System.out.println("Hello World...");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		greeter.greet();
+
+	}
+
+}

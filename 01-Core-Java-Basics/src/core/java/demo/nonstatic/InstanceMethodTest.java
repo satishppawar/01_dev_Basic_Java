a7125b6ec32e6bb5d9bf2cda1ab6af37d5499ca4
satishppawar <pawar.satish2345@gmail.com>
2020-07-25 09:16:24 +0530

    Old changes

---
+++ 01-Core-Java-Basics/src/core/java/demo/nonstatic/InstanceMethodTest.java
@@ -0,0 +1,32 @@
+package core.java.demo.nonstatic;
+
+public class InstanceMethodTest {
+	// instance variable
+	int instanceVar = 10;
+
+	// Static var or class variables
+	static String staticVar = "Welcome";
+
+	/*
+	 * Instance methods can access instance variables and instance methods directly.
+	 * 
+	 * Instance methods can access class variables and class methods directly.
+	 */
+	public void instanceMethod1() {
+		System.out.println("instanceMethod1 is called.");
+		System.out.println("instanceVar : " + instanceVar);
+		instanceMethod2();
+
+		System.out.println("staticVar : " + staticVar);
+		staticMethod();
+	}
+
+	public void instanceMethod2() {
+		System.out.println("instanceMethod2 is called.");
+	}
+
+	public static void staticMethod() {
+		System.out.println("staticMethod is called.");
+	}
+
+}
---
+++ src/main/java/StringTrimmer.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+
+public class StringTrimmer {
+    public static void main (String[] args) throws java.lang.Exception {
+        String s1 = "ExpirartionDateRule-Vm1_disk1_d4e340863d4c46a782f332d330d69fd5";
+        String s2 = "abcdefghiojkabcdefghiojkabcdefghiojkabcdedisk1_e5bb14a616484f20a492178e03407ab5";
+       System.out.println("After removing s1==" + removeLastChar(s1) + s1.length());
+        System.out.println("After removing s2== " + removeLastChar(s2)+s2.length() );
+
+    }
+
+    private static String removeLastChar(String str) {
+       String prefixStr=str.substring(0, str.length() - 32);
+       String suffixStr=str.substring(str.length()-4);
+       System.out.println("prefixStr is"+prefixStr);
+       System.out.println("suffixStr is"+suffixStr);
+       String s=prefixStr+"xxxx"+suffixStr;
+       System.out.println("-------------");
+       return s;
+    }
+}

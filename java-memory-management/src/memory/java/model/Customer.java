b5b1bcaebe5d21a80059705048f7457fddf9508a
satishppawar <pawar.satish2345@gmail.com>
2020-08-08 23:52:29 +0530

    excecise1

---
+++ java-memory-management/src/memory/java/model/Customer.java
@@ -0,0 +1,18 @@
+package memory.java.model;
+
+public class Customer {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public Customer(String name) {
+		super();
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

90eea78a682408fa95711318df5919c4b5a8b011
satishppawar <pawar.satish2345@gmail.com>
2020-10-02 10:34:41 +0530

    Basic datatypes

---
+++ 02-Complete-Java-Certification-Course/01-Java-Basic/src/com/java_basic/Variables.java
@@ -0,0 +1,29 @@
+package com.java_basic;
+
+/*
+ * Reference link
+ * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+ */
+public class Variables {
+	public static void main(String[] args) {
+
+		int intVar = 1000000000;
+		short smallNum = 3413;
+		byte reallySmall = -128;
+		double decimalNum = 434.434;
+		boolean decision = false;
+		char letter = '$';
+
+		// Java is strongly typed language
+
+		// double i="dwfd";
+
+		System.out.println("Demo of the varibles");
+		System.out.println("Int var is" + intVar);
+		System.out.println("smallNum is" + smallNum);
+		System.out.println("reallySmall is" + reallySmall);
+		System.out.println("decimalNum is" + decimalNum);
+		System.out.println("decision is" + decision);
+		System.out.println("letter is" + letter);
+	}
+}
